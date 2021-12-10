071cc1c02ac0830e96a6882edddba02aa2d184df
EL HAJRAOUI Salma <salma.elhajraoui@docaposte.fr>
2021-12-10 18:09:37 +0100

    ui in progress

--- src/views/search/Search.vue
+++ src/views/search/Search.vue
@@ -1,16 +1,23 @@
 <template>
-  <el-row>
-    <el-col :span="12">
-      Ex consectetur excepteur consectetur ut eu dolor excepteur culpa. Velit adipisicing laborum quis ea aute. Ullamco tempor duis esse laborum proident aute. Ex et minim minim nostrud sit eu consequat sint labore pariatur amet. Amet laboris exercitation irure Lorem ea do veniam anim aliquip id proident sit adipisicing amet.
-    </el-col>
-    <el-col :span="12">
-      <el-input
-        @keyup.enter="searchUniverse"
-        v-model="username"
-        placeholder="Please enter your username"
-      />
-    </el-col>
-  </el-row>
+  <section class="bg-white dark:bg-gray-800">
+    <div class="max-w-3xl px-6 py-16 mx-auto text-center">
+      <h1 class="text-3xl font-semibold text-gray-800 dark:text-gray-100">Lorem ipsum dolor sit amet</h1>
+      <p class="max-w-md mx-auto mt-5 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, minus tempora nemo quos</p>
+
+      <div class="flex flex-col mt-8 space-y-3 sm:space-y-0 sm:flex-row sm:justify-center sm:-mx-2">
+        <input
+          id="username"
+          type="text"
+          class="px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md sm:mx-2 dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+          placeholder="Username"
+        >
+        <button class="px-4 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-700 rounded-md sm:mx-2 hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
+          Search
+        </button>
+      </div>
+    </div>
+  </section>
+
 </template>
 
 <script>
